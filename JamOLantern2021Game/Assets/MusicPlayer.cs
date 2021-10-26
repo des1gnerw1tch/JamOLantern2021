f6635ad42442ab7c4bfe3a37035058490c1c599a
des1gnerw1tch <zachcanon@gmail.com>
2021-10-26 16:27:20 -0400

    Audio stuff

---
+++ JamOLantern2021Game/Assets/MusicPlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Deals with playing music in game
+public class MusicPlayer : MonoBehaviour {
+	[SerializeField] private string songName; // name in AudioManager
+
+	// Start is called before the first frame update
+	void Start () {
+		FindObjectOfType<AudioManager> ().Play (this.songName); // plays our song for us
+	}
+}
---
+++ JamOLantern2021Game/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+	public Sound [] sounds;
+	// Start is called before the first frame update
+	void Awake () {
+		foreach (Sound s in sounds) {
+			s.source = gameObject.AddComponent<AudioSource> ();
+			s.source.clip = s.clip;
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.loop;
+		}
+	}
+
+	//this is for sound effects
+	public void Play (string name) {
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		s.source.Play ();
+	}
+
+	// this is to play sound effect with certain pitch
+	public void Play (string name, float pitch) {
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		s.source.pitch = pitch;
+		s.source.Play ();
+	}
+
+	// resets the pitch of a audio source
+	public void ResetPitch (string name) {
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		s.source.pitch = s.pitch;
+	}
+
+	//this is to play music
+	public void PlayTheme (string name) {
+		//if (PlayerSettings.music)  {
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		s.source.Play ();
+		//  }
+	}
+
+	public void Stop (string name) {
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		s.source.Stop ();
+	}
+
+	public void Pause (string name) {
+		Sound s = Array.Find (sounds, sound => sound.name == name);
+		s.source.Pause ();
+	}
+}
