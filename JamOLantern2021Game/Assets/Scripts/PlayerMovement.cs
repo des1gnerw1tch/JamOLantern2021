e3a70318a01ac8cdb9e37a925c5187262dc69ff7
chloecwn <93173038+chloelocious@users.noreply.github.com>
2021-10-26 19:46:06 -0400

    Update PlayerMovement.cs

--- JamOLantern2021Game/Assets/Scripts/PlayerMovement.cs
+++ JamOLantern2021Game/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Transform toMove;
-    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private float horizontalMovement;
+    [SerializeField] private float verticalMovement;
+    [SerializeField] private float sprintSpeed = 10.0f;
+    [SerializeField] private float speedDampener = 0.7f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown("w")) {
-            this.toMove.Translate(Vector3.up * Time.deltaTime * this.speed);
-        Debug.Log("Moved Forward");
-      }
+        // - 1 = left, 1 = right
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+
+        // -1 = down, 1 = up
+        verticalMovement = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // diagonal movement
+        if (horizontalMovement != 0 && verticalMovement != 0)
+        {
+            horizontalMovement += speedDampener;
+            verticalMovement += speedDampener;
+        }
+
+        body.velocity = new Vector2(horizontalMovement * sprintSpeed,
+            verticalMovement * sprintSpeed);
     }
 }
