5ceca63a53476ea262a31f41e95784ae46c8cb40
des1gnerw1tch <zachcanon@gmail.com>
2021-10-30 03:01:22 -0400

    Cards can dispense on use, flip through cards,

--- JamOLantern2021Game/Assets/Scripts/Card.cs
+++ JamOLantern2021Game/Assets/Scripts/Card.cs
@@ -7,6 +7,7 @@ public abstract class Card : MonoBehaviour {
 	[SerializeField] private int level; // level of this card
 	[SerializeField] private GameObject CanvasPrefab; //
 	public Sprite sprite;
+	public bool dispenseWhenUsed;
 
 	// xDirection: The direction the player is facing on the x axis
 	// yDirection: The direction the player is facing on the y axis
--- JamOLantern2021Game/Assets/Scripts/PlayerCards.cs
+++ JamOLantern2021Game/Assets/Scripts/PlayerCards.cs
@@ -9,7 +9,9 @@ using TMPro;
 public class PlayerCards : MonoBehaviour {
 
 	[SerializeField] private List<Card> cards; // list of Card's in inventory, should all be prefabs
-	[SerializeField] private KeyCode keyToAttack;
+	[SerializeField] private KeyCode keyToUse; // Key pressed to use card
+	[SerializeField] private KeyCode keyToSkip; // Key pressed to skip card
+
 	[SerializeField] private PlayerMovement playerMovement; // Player movement of this player
 	[SerializeField] private Image currentActiveCardImage; // the active UI card seen on the canvas for this player
 	[SerializeField] private TextMeshProUGUI currentNumText; //UI Element
@@ -19,32 +21,66 @@ public class PlayerCards : MonoBehaviour {
 
 	private void Start () {
 		this.currentCardIndex = 0;
-		this.UpdateCurrentCardImage ();
-	}
-
-	// updates the current card image UI
-	private void UpdateCurrentCardImage () {
-		this.currentActiveCardImage.sprite = this.cards [this.currentCardIndex].sprite;
-		this.currentNumText.text = this.currentCardIndex + 1 + "";
-		this.deckSizeText.text = "/ " + this.cards.Count;
+		this.UpdateCardUI ();
 	}
 
 	// called every frame
 	private void Update () {
 		// To activate card
-		if (Input.GetKeyDown (this.keyToAttack)) {
+		if (Input.GetKeyDown (this.keyToUse)) {
+			this.UseCard ();
+		}
+
+		// Skip a card
+		if (Input.GetKeyDown (this.keyToSkip)) {
+			this.IncCurrentIndex ();
+			this.UpdateCardUI ();
+		}
+	}
+
+	// Goes to next card in deck, wraps around if needed
+	void IncCurrentIndex () {
+		// go to next card in deck
+		if (this.currentCardIndex < this.cards.Count - 1) {
+			this.currentCardIndex++;
+		} else {
+			this.currentCardIndex = 0;
+		}
+	}
+
+	// Uses current card
+	void UseCard () {
+		// Does the deck have cards? 
+		if (this.cards.Count > 0) {
 			this.cards [this.currentCardIndex].Activate ((int)this.playerMovement.playerFacing.x,
 				(int)this.playerMovement.playerFacing.y, this.transform.position);
-
-			// go to next card in deck
-			if (this.currentCardIndex < this.cards.Count - 1) {
-				this.currentCardIndex++;
-			} else {
-				this.currentCardIndex = 0;
+			if (this.cards [this.currentCardIndex].dispenseWhenUsed) { // if dispensed when used
+				cards.RemoveAt (currentCardIndex); // remove from stack
+				if (this.cards.Count == currentCardIndex) { // are we at our last card in teh deck?
+					IncCurrentIndex ();
+					//this.currentCardIndex--;
+				}
+			} else { // if not dispensed when used
+				this.IncCurrentIndex (); // go to next card in deck
 			}
 
-			this.UpdateCurrentCardImage ();
-
+			this.UpdateCardUI ();
+		} else { // the deck has no cards
+				 //TODO: Add negative noise here
 		}
+
+	}
+
+	// updates the current card image UI
+	private void UpdateCardUI () {
+
+		// is Deck empty? 
+		if (this.cards.Count <= 0) {
+			this.currentActiveCardImage.sprite = null;
+		} else {
+			this.currentActiveCardImage.sprite = this.cards [this.currentCardIndex].sprite;
+		}
+		this.currentNumText.text = this.currentCardIndex + 1 + "";
+		this.deckSizeText.text = "/ " + this.cards.Count;
 	}
 }
