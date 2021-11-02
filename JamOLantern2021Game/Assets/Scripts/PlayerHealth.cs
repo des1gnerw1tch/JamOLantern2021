bfd4bc834ab0e14335c44bda773ec74bf3861aaa
Tomás Carlson <34401946+TomasCarlson@users.noreply.github.com>
2021-11-01 20:00:57 -0400

    Health

--- JamOLantern2021Game/Assets/Scripts/PlayerHealth.cs
+++ JamOLantern2021Game/Assets/Scripts/PlayerHealth.cs
@@ -4,4 +4,26 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour {
 	[SerializeField] private int health;
+
+	void SartHealth () {
+		this.health = 100;
+	}
+
+	// Returns true if still alive, false if not
+	boolean Damage (int healthPoints) {
+		this.health -= healthPoints;
+		return health <= 0;
+	}
+
+	// Adds Health by points, no overheal
+	void Health (int healthPoints) {
+		if (health < 100) {
+			this.health = Math.Min(100, this.health + healthPoints);
+		}
+	}
+
+	// Adds Health by points, with overheal to 120
+	void HealthOverheal (int healthPoints) {
+		this.health = Math.Min(120, this.health + healthPoints);
+	}
 }
