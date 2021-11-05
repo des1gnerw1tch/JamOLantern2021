511ad00202b74126a414385d2cbbc1414f1b27f8
chloecwn <93173038+chloelocious@users.noreply.github.com>
2021-11-05 18:54:04 -0400

    End scene transition 2

--- JamOLantern2021Game/Assets/EndGame.cs
+++ JamOLantern2021Game/Assets/EndGame.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGame : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+   {
+        KeepOnSceneChange[] objs = FindObjectsOfType<KeepOnSceneChange>();
+
+        foreach (KeepOnSceneChange obj in objs)
+        {
+            Destroy(obj.gameObject);
+        }
+        SceneManager.LoadScene("PlayerSelect");
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
