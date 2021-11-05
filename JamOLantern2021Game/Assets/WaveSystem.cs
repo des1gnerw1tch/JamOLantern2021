1ea95299a8cb82fa2ecfe358306d85e8b3f4f89b
des1gnerw1tch <zachcanon@gmail.com>
2021-11-05 16:58:32 -0400

    new Wave UI

--- JamOLantern2021Game/Assets/WaveSystem.cs
+++ JamOLantern2021Game/Assets/WaveSystem.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WaveSystem : MonoBehaviour {
 	[SerializeField] private Wave [] waves;
 	private int waveOn;
 	[SerializeField] private GameObject stairs; // stairs to go to next floor
 												// Start is called before the first frame update
+	private TextMeshProUGUI waveCountUI;
+
 	void Start () {
 		this.waveOn = 0;
+		this.waveCountUI = GameObject.FindWithTag ("waveCounter").GetComponent<TextMeshProUGUI> ();
 
 		foreach (Wave wave in waves) {
 			wave.gameObject.SetActive (false);
@@ -18,6 +22,7 @@ public class WaveSystem : MonoBehaviour {
 		this.waves [this.waveOn].ActivateWave ();
 		this.waves [this.waveOn].gameObject.SetActive (true);
 		this.stairs.SetActive (false);
+		UpdateWaveCountUI ();
 	}
 
 
@@ -26,13 +31,20 @@ public class WaveSystem : MonoBehaviour {
 		if (waveOn >= waves.Length) {
 			this.ActivateStairs ();
 			return;
+		} else {
+			FindObjectOfType<AudioManager> ().Play ("nextWave");
 		}
 		this.waves [this.waveOn].gameObject.SetActive (true);
 		this.waves [this.waveOn].ActivateWave ();
+		UpdateWaveCountUI ();
 	}
 
 	private void ActivateStairs () {
 		FindObjectOfType<AudioManager> ().Play ("spawnStairs");
 		this.stairs.SetActive (true);
+	}
+
+	private void UpdateWaveCountUI () {
+		this.waveCountUI.text = waveOn + 1 + "";
 	}
 }
