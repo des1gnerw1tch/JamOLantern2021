5d50ec5987aff2c2e6c606ffc54255c056c06c79
des1gnerw1tch <zachcanon@gmail.com>
2021-11-04 23:39:12 -0400

    Explosive Round Card finished

--- JamOLantern2021Game/Assets/ExplosiveRound.cs
+++ JamOLantern2021Game/Assets/ExplosiveRound.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FriendlyProjectile : MonoBehaviour {
-	[SerializeField] protected int damage;
-	[SerializeField] protected bool pierce;
-
-	public virtual void OnTriggerEnter2D (Collider2D other) {
+// A friendly projectile that is explosive
+public class ExplosiveRound : FriendlyProjectile {
+	[SerializeField] private GameObject explosion;
+	public override void OnTriggerEnter2D (Collider2D other) {
+		//base.OnTriggerEnter2D (other);
 		if (other.gameObject.CompareTag ("Enemy")) {
 			if (other.gameObject.GetComponent<EnemyHealth> ().Damage (this.damage)) { // if enemy dies
 				other.gameObject.GetComponent<Enemy> ().DropCard ();
 				Destroy (other.gameObject);
 				FindObjectOfType<AudioManager> ().Play ("enemyDeath");
+
 			} else {
 				FindObjectOfType<AudioManager> ().Play ("damageToEnemy");
 			}
+
+			Instantiate (this.explosion, new Vector3 (this.transform.position.x, this.transform.position.y,
+				this.transform.position.z), Quaternion.identity);
 
 			if (!pierce)
 				Destroy (this.gameObject);
